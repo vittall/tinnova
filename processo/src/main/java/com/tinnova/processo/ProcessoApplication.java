ee6852446215522af1b9df5da1405c5c02b0cac2
victo <victo@192.168.0.13>
2020-10-05 15:32:49 -0300

    commit inicial

---
+++ processo/src/main/java/com/tinnova/processo/ProcessoApplication.java
@@ -0,0 +1,13 @@
+package com.tinnova.processo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessoApplication.class, args);
+	}
+
+}
---
+++ processo/src/test/java/com/tinnova/processo/ProcessoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinnova.processo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
